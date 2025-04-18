bc65af7caf5c430dcc71614b1763e71268375224
Kavya <kolkavya@deloitte.com>
2025-04-18 11:10:30 +0530

    first commit

---
+++ calculator/calculator.js
@@ -0,0 +1,17 @@
+const calculate = (num1, num2, operator) => {
+  if (operator === "+") {
+    return num1 + num2;
+  } 
+  else if (operator === "-") {
+    return num1 - num2;
+  } 
+  else if (operator === "*") {
+    return num1 * num2;
+  } 
+  else {
+    return num1 / num2;
+  }
+};
+
+
+module.exports = calculate;
---
+++ calculator/test.js
@@ -0,0 +1,22 @@
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => response.json())
+//       .then((json) => console.log(json))
+//       .catch((error) => alert("landing error!"));
+// console.log("Hello World");
+// let a = 10;
+// let num = 20;
+
+// function greeting(num1, num2) {
+//   return num1 + num2;
+// }
+// //Your number 1: 10 + Your number 2: 20 = 30
+// let result = greeting(a, num);
+// console.log("Your number 1 :" + a + " Your number 2 : " + num + "=" + result);
+// console.log(`Your number 1: ${a} + Your number 2: ${num} = ${result}`);
+const calculator = require('./calculator')
+let num1 = 10;
+let num2 = 20;
+let operator = '*';
+console.log(calculator(num1,num2,operator))
+
+
